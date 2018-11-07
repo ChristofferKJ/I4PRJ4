98c69c2810ed731141526c5e57d09f71af223841
Hindkaer <hindkaer_msn@hotmail.com>
2018-11-07 14:23:06 +0100

    Der kan nu læses og skrives til databasen

--- App1/App1/App1/ViewModel/QuizViewModel.cs
+++ App1/App1/App1/ViewModel/QuizViewModel.cs
@@ -18,12 +18,20 @@ namespace App1.ViewModel
         private bool _isNew;
         public event EventHandler SaveComplete;
 
+        List<Quiz> myQuizzes;
+        public List<Quiz> MyQuizzes { get => myQuizzes; set => SetProperty(ref myQuizzes, value); }
+        public ICommand RefreshCommand { get; }
+
         public QuizViewModel(Quiz theQuiz, bool isNew)
         {
             _isNew = isNew;
             TheQuiz = theQuiz;
             Title = isNew ? "New Quiz" : TheQuiz.Category;
             SaveCommand = new Command(async () => await ExecuteSaveCommand());
+
+            MyQuizzes = new List<Quiz>();
+            RefreshCommand = new Command(async () => await ExecuteRefreshCommand());
+            IsBusy = false;
         }
 
         async Task ExecuteSaveCommand()
@@ -36,6 +44,24 @@ namespace App1.ViewModel
                 await quizServices.UpdateTask(TheQuiz);
 
             SaveComplete?.Invoke(this, new EventArgs());
+        }
+
+        async Task ExecuteRefreshCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                var quizServices = new QuizServices();
+                MyQuizzes = await quizServices.GetAllQuizzes();
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
     }
 
