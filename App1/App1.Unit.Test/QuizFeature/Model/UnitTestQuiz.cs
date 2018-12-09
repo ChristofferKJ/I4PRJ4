65e9568a8fee45b572bf3683457ed9f0ff68e8e0
Kasper Andersen <kga@live.dk>
2018-12-09 13:16:15 +0100

    updated

--- App1/App1.Unit.Test/QuizFeature/Model/UnitTestQuiz.cs
+++ App1/App1.Unit.Test/QuizFeature/Model/UnitTestQuiz.cs
@@ -8,8 +8,7 @@ using NUnit.Framework;
 
 namespace App1.Unit.Test.QuizFeature.Model
 {
-
-    /*
+    
     [TestFixture]
     class UnitTestQuiz
     {
@@ -22,65 +21,8 @@ namespace App1.Unit.Test.QuizFeature.Model
             uut_ = new Quiz();
         }
 
-        [Test]
-        public void OnPro()
-        {
-            int propertychangedCounter = 0;
-            uut_.PropertyChanged += (o, arg) => { propertychangedCounter++; };
+       
 
-            
-
-            Assert.That(propertychangedCounter, Is.EqualTo(1));
-            Assert.That(uut_.IsRight, Is.EqualTo(false));
-
-        }
-
-        [Test]
-        public void OnPro2()
-        {
-            int propertychangedCounter = 0;
-            uut_.PropertyChanged += (o, arg) => { propertychangedCounter++; };
-
-            uut_.IsRight = false;
-            uut_.IsRight = false;
-
-            Assert.That(propertychangedCounter == 2, Is.EqualTo(false));
-            Assert.That(propertychangedCounter == 1, Is.EqualTo(true));
-            Assert.That(uut_.IsRight, Is.EqualTo(false));
-
-        }
-
-        [Test]
-        public void OptionTextChangedOnceToNewString()
-        {
-            int propertychangedCounter = 0;
-            uut_.PropertyChanged += (o, arg) => { propertychangedCounter++; };
-
-            string answer = "anwser";
-            uut_.OptionText = answer;
-
-            Assert.That(propertychangedCounter, Is.EqualTo(1));
-            Assert.That(uut_.OptionText, Is.EqualTo(answer));
-
-        }
-
-        [Test]
-        public void OptionTextChangedTwiceToSameString()
-        {
-            int propertychangedCounter = 0;
-            uut_.PropertyChanged += (o, arg) => { propertychangedCounter++; };
-
-            string answer = "anwser";
-            uut_.OptionText = answer;
-            uut_.OptionText = answer;
-
-            Assert.That(propertychangedCounter == 2, Is.EqualTo(false));
-            Assert.That(propertychangedCounter == 1, Is.EqualTo(true));
-            Assert.That(uut_.OptionText, Is.EqualTo(answer));
-        }
-
-        
-
-    } */
+    } 
 
 }
---
+++ App1/App1.Unit.Test/QuizFeature/ViewModel/QuizViewModelUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1.ViewModel;
+using NUnit.Framework;
+
+namespace App1.Unit.Test.QuizFeature.ViewModel
+{
+    [TestFixture]
+        class QuizViewModelUnitTest
+    {
+        private QuizViewModel uut_;
+
+        [SetUp]
+
+        void setup()
+        {
+
+        }
+    }
+}
